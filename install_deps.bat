104860235477fc46aa33c31c81a0541241870a63
Hanger <hanger3817@gmail.com>
2025-05-08 17:01:32 +0300

    Add install_deps.bat script

---
+++ install_deps.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Установка зависимостей из requirements.txt...
+pip install -r requirements.txt
+
+if %ERRORLEVEL% NEQ 0 (
+    echo [ERROR] При установке зависимостей произошёл сбой. Пожалуйста повторите попытку
+    exit /b %ERRORLEVEL%
+) else (
+    echo [SUCCESS] Все зависимости успешно установлены! Можете запускать flask > app.py
+)
+pause
