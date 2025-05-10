c54ee46d4ff829f97934770ea7389d76ef4f9df8
Hanger <hanger3817@gmail.com>
2025-05-10 14:01:52 +0300

    Simple anime list v 0.6 bug fix 3.1

--- static/js/script.js
+++ static/js/script.js
@@ -182,7 +182,7 @@ async function changeUrl(name) {
 
 function changePage(page) {
   currentPage = page;
-  fetchAnimeList();
+  updateAnimeListOverall();
 }
 //------------------------------------------------------------------------------------------
 
